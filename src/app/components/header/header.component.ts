ab303e1619312249a28f81272ae5cb28eaa1844c
marcosprofile <marcos_laurindo1997@hotmail.com>
2024-05-28 00:31:07 -0300

    feat: created new menu from mobile responsive

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  menuElement: HTMLElement | null = null
 
+  chats = [
+    {
+      id: 1,
+      text: "Esta é minha primeira pesquisa no Gemini",
+      class: "more-action"
+    },
+    {
+      id: 2,
+      text: "Esta é minha segunda pesquisa no Gemini",
+      class: "more-action"
+    },
+    {
+      id: 3,
+      text: "Esta é minha terceira pesquisa no Gemini",
+      class: "more-action"
+    },
+    {
+      id: 4,
+      text: "Esta é minha quarta pesquisa no Gemini",
+      class: "more-action"
+    },
+    {
+      id: 5,
+      text: "Esta é minha quinta pesquisa no Gemini",
+      class: "more-action"
+    },
+  ]
+
+  actions = [
+    {
+      id: 1,
+      icon: "contact_support",
+      text: "Ajuda",
+      class: "new"
+    },
+    {
+      id: 2,
+      icon: "history",
+      text: "Atividades",
+      class: ""
+    },
+    {
+      id: 3,
+      icon: "settings",
+      text: "Configurações",
+      class: ""
+    },
+  ]
+
+  ngOnInit() {
+    this.menuElement = document.querySelector('.mobile')
+  }
+
+  abrirMenu() {
+    if (this.menuElement) {
+      this.menuElement.classList.toggle('closed')
+    }
+  }
+
+  fecharMenu() {
+    if(this.menuElement) {
+      this.menuElement.classList.remove('closed')
+    }
+  }
 }
